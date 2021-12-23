9e1a081a6aba34fd97c64ea5c2ae9baf1070e896
Edward Boado <Eboado8862@conestogac.on.ca>
2021-12-22 20:01:14 -0500

    Updated nav bar dynamic resizing and button slide menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
-function displayImage()
-{
-  document.getElementById("image").style.display="block";
+
+// Toggle the nav bar button menu
+function toggleNav() {
+  var navButton = document.getElementById('navImage');
+
+  // Check which image source is present
+  if(navButton.src.match("images/navButton.png")) {
+    navButton.src = "images/navX.png";
+    document.getElementById('nav').className = "nav-vertical";
+  }
+  else {    
+    navButton.src = "images/navButton.png";
+    document.getElementById('nav').className = "nav-horizontal";
+  }
+
 }
