c6a506754d353de19497f7a9a6eba0d3dc70b814
Edward Boado <Eboado8862@conestogac.on.ca>
2021-12-23 10:20:57 -0500

    Updated directory file structure

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,12 @@ function toggleNav() {
   var navButton = document.getElementById('navImage');
 
   // Check which image source is present
-  if(navButton.src.match("images/navButton.png")) {
-    navButton.src = "images/navX.png";
+  if(navButton.src.match("https://storage.googleapis.com/edwardboado.dev/images/navButton.png")) {
+    navButton.src = "https://storage.googleapis.com/edwardboado.dev/images/navX.png";
     document.getElementById('nav').className = "nav-vertical";
   }
   else {    
-    navButton.src = "images/navButton.png";
+    navButton.src = "https://storage.googleapis.com/edwardboado.dev/images/navButton.png";
     document.getElementById('nav').className = "nav-horizontal";
   }
 
